d9eb14966b6ac6141c0f0ca0e9b48284164280b3
berkchy <berkchcode@gmail.com>
2026-07-29 15:51:36 +0300

    manual step-by-step build with logging to find hang

--- js/plugins/models/model_ak47.js
+++ js/plugins/models/model_ak47.js
@@ -67,25 +67,137 @@ plugin.register({
     console.log('[model_ak47] _build starting');
     var mdl = plugin.get('model_mdl');
     if (!mdl) return new THREE.Group();
-    var r;
-    try {
-      r = mdl.build(this._mdlFile);
-    } catch(e) {
-      console.error('[model_ak47] build ERROR:', e.message, e.stack);
-      return new THREE.Group();
-    }
-    var g = r.group;
-    g.name = 'ak47_model';
+    var mdlFile = this._mdlFile;
+    console.log('[model_ak47] mdlFile bones:', mdlFile.bones.length, 'bodyParts:', mdlFile.bodyParts.length, 'textures:', mdlFile.textures.length, 'sequences:', mdlFile.sequences.length);
 
-    var mixer = new THREE.AnimationMixer(g);
+    // Manual build step by step to isolate hang
+    var THREE = window.THREE;
+    var group = new THREE.Group();
+    group.name = '[ROOT]';
+
+    console.log('[model_ak47] creating bones...');
+    var srcbones = mdlFile.bones;
+    var bones = [];
+    for (var i = 0; i < srcbones.length; i++) {
+      var a = srcbones[i];
+      var b = new THREE.Bone();
+      b.position.set(a.value[0], a.value[1], a.value[2]);
+      b.rotation.set(a.value[3], a.value[4], a.value[5], 'ZYX');
+      b.name = a.name;
+      bones.push(b);
+    }
+    console.log('[model_ak47] building hierarchy...');
+    for (var i = 0; i < srcbones.length; i++) {
+      for (var j = 0; j < srcbones.length; j++) {
+        if (srcbones[j].parent === i) { bones[i].add(bones[j]); }
+      }
+    }
+    var boneGroup = new THREE.Group();
+    boneGroup.name = '[BONE]';
+    for (var i = 0; i < srcbones.length; i++) {
+      if (srcbones[i].parent === -1) boneGroup.add(bones[i]);
+    }
+    group.add(boneGroup);
+    console.log('[model_ak47] creating skeleton...');
+    var skeleton = new THREE.Skeleton(bones);
+
+    console.log('[model_ak47] creating textures...');
+    var textures = [];
+    mdlFile.textures.forEach(function(a) {
+      var b = new THREE.DataTexture(a.data, a.width, a.height, THREE.RGBAFormat, THREE.UnsignedByteType,
+        THREE.UVMapping, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping, THREE.LinearFilter, THREE.LinearFilter);
+      b.name = a.name;
+      b.needsUpdate = true;
+      textures.push(b);
+    });
+
+    console.log('[model_ak47] building meshes...');
+    var bodyGroup = new THREE.Group();
+    bodyGroup.name = '[BODY]';
+    var totalMeshes = 0;
+    mdlFile.bodyParts.forEach(function(a) {
+      var partGroup = new THREE.Group();
+      partGroup.name = '[PART]';
+      a.models.forEach(function(b) {
+        var position = [], normal = [], uv = [], skinIndex = [], skinWeight = [];
+        for (var vi = 0; vi < b.vertices.length; vi++) {
+          var c = b.vertices[vi];
+          position.push(c.position.x, c.position.y, c.position.z);
+          normal.push(c.normal.x, c.normal.y, c.normal.z);
+          uv.push(c.texCoord.x, c.texCoord.y);
+          skinIndex.push(c.bone, 0, 0, 0);
+          skinWeight.push(1, 0, 0, 0);
+        }
+        var aPosition = new THREE.Float32BufferAttribute(position, 3);
+        var aNormal = new THREE.Float32BufferAttribute(normal, 3);
+        var aUV = new THREE.Float32BufferAttribute(uv, 2);
+        var aSkinIndex = new THREE.Uint8BufferAttribute(skinIndex, 4);
+        var aSkinWeight = new THREE.Float32BufferAttribute(skinWeight, 4);
+
+        var meshGroup = new THREE.Group();
+        meshGroup.name = b.name;
+        b.mesh.forEach(function(c) {
+          var geo = new THREE.BufferGeometry();
+          geo.setAttribute('position', aPosition);
+          geo.setAttribute('normal', aNormal);
+          geo.setAttribute('uv', aUV);
+          geo.setAttribute('skinIndex', aSkinIndex);
+          geo.setAttribute('skinWeight', aSkinWeight);
+          geo.setIndex(c.indices);
+
+          var mat = new THREE.MeshBasicMaterial({
+            map: textures[mdlFile.skinfamilies[0][c.skinref]],
+            skinning: true,
+            side: THREE.DoubleSide
+          });
+          var mesh = new THREE.SkinnedMesh(geo, mat);
+          mesh.bind(skeleton);
+          meshGroup.add(mesh);
+          totalMeshes++;
+        });
+        partGroup.add(meshGroup);
+      });
+      bodyGroup.add(partGroup);
+    });
+    group.add(bodyGroup);
+    console.log('[model_ak47] meshes built:', totalMeshes);
+
+    console.log('[model_ak47] rotating bone group...');
+    boneGroup.rotateX(-1.570796);
+
+    console.log('[model_ak47] creating animations...');
+    var animations = [];
+    mdlFile.sequences.forEach(function(a) {
+      var duration = a.frames.length / a.fps;
+      var tracks = [];
+      for (var i = 0; i < srcbones.length; i++) {
+        var timeval = [];
+        var posvals = [];
+        var rotvals = [];
+        for (var j = 0; j < a.frames.length; j++) {
+          timeval.push(j / a.frames.length * duration);
+          posvals.push(a.frames[j].pos[i].x, a.frames[j].pos[i].y, a.frames[j].pos[i].z);
+          rotvals.push(a.frames[j].rot[i].x, a.frames[j].rot[i].y, a.frames[j].rot[i].z, a.frames[j].rot[i].w);
+        }
+        var posTrack = new THREE.VectorKeyframeTrack(srcbones[i].name + '.position', timeval, posvals);
+        var rotTrack = new THREE.QuaternionKeyframeTrack(srcbones[i].name + '.quaternion', timeval, rotvals);
+        tracks.push(posTrack, rotTrack);
+      }
+      var clip = new THREE.AnimationClip(a.label, duration, tracks);
+      animations.push(clip);
+    });
+    console.log('[model_ak47] animations created:', animations.length);
+
+    group.name = 'ak47_model';
+    var mixer = new THREE.AnimationMixer(group);
     var clips = {};
-    for (var i = 0; i < r.animations.length; i++) {
-      var c = r.animations[i];
+    for (var i = 0; i < animations.length; i++) {
+      var c = animations[i];
       clips[c.name] = c;
     }
-    g.userData.mixer = mixer;
-    g.userData.clips = clips;
-    console.log('[model_ak47] _build done, meshes:', g.getObjectsByProperty('isMesh', true).length);
-    return g;
+    group.userData.mixer = mixer;
+    group.userData.clips = clips;
+    console.log('[model_ak47] _build done');
+    return group;
   }
 });
